3551c1eeb75c5a59714cf171e740bc7071ceb8e9
최예린 <qsc130@gmail.com>
2022-11-17 17:47:11 +0900

    기본 뼈대 완성, navbar 작성 중

---
+++ final-pjt-front/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+    <h1>HomeView</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/templates/NavBar'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+</style>
