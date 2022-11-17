1de3ff6f3323bcf6f628f41a2a4d50e7cdf0f6e3
최예린 <qsc130@gmail.com>
2022-11-18 02:25:04 +0900

    navbar 다시 수정

--- final-pjt-front/src/views/SearchView.vue
+++ final-pjt-front/src/views/SearchView.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
+    <nav-bar></nav-bar>
     <h1>SearchView</h1>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/templates/NavBar'
 
 export default {
   name: 'SearchView',
+  components: {
+    NavBar
+  },
 }
 </script>
 
