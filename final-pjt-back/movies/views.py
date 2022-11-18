048a3ad336906772c232749bc6878c72e7eb4618
boham97 <kangbosung97@gmail.com>
2022-11-18 10:56:19 +0900

    movie_list data gogo

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework import status
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .serializers import MovieListSerializer
+from .models import Movie
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def movie_list(request):
+    if request.method == 'GET':
+        Movies = get_list_or_404(Movie)
+        serializer = MovieListSerializer(Movies, many=True)
+        return Response(serializer.data)
