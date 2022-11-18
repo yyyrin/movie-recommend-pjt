0c559aa8eb97a82cd8f6260e3f048b8cef428288
최예린 <qsc130@gmail.com>
2022-11-18 14:29:00 +0900

    store에서 수정하다가 막혔어요ㅠ

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,81 +1,12 @@
-import router from '@/router'
-import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
+
+import accounts from '@/store/modules/accounts'
 
 Vue.use(Vuex)
 
-const API_URL = 'http://127.0.0.1:8000'
-
 export default new Vuex.Store({
-  plugins: [
-    createPersistedState()
-  ],
-  state: {
-    movies: [],
-    token: null,
-  },
-  getters: {
-    isLogin(state) {
-      return state.token ? true : false
-    }
-  },
-  mutations: {
-    GET_MOVIES(state, movies) {
-      state.movies = movies
-    },
-    // 회원가입 && 로그인
-    SAVE_TOKEN(state, token) {
-      state.tojken = token
-      router.push({ name: 'home' })
-    }
-  },
-  actions: {
-    getMovies(context) {
-      axios({
-        method: 'get',
-        url: `${API_URL}/api/v1/movies/`,
-        headers: {
-          Authorization: `Token ${context.state.token}`
-        }
-      })
-      .then((res) => {
-        context.commit('GET_MOVIES', res.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    },
-    signUp(context, payload) {
-      axios({
-        method: 'post',
-        url: `${API_URL}/accounts/signup/`,
-        data: {
-          username: payload.username,
-          password1: payload.password1,
-          password2: payload.password2,
-        }
-      })
-      .then((res) => {
-        context.commit('SAVE_TOKEN', res.data.key)
-      })
-    },
-    logIn(context, payload) {
-      axios({
-        method: 'post',
-        url: `${API_URL}/accounts/login/`,
-        data: {
-          username: payload.username,
-          password: payload.password,
-        }
-      })
-      .then((res) => {
-        // console.log(res)
-        context.commit('SAVE_TOKEN', res.data.key)
-      })
-    }
-  },
   modules: {
+    accounts,
   }
 })
