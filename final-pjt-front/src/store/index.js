acc3f486750b6069eebd85339d90777cd88bc427
최예린 <qsc130@gmail.com>
2022-11-18 14:49:11 +0900

    HomeView 수정 및 store 폴더 복구

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,12 +1,97 @@
+import router from '@/router'
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-import accounts from '@/store/modules/accounts'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
+const API_URL = 'http://127.0.0.1:8000'
+
 export default new Vuex.Store({
+  // 토큰관리 -> Local에 저장 -> 나중에 변경하기
+  plugins: [
+    createPersistedState()
+  ],
+  state: {
+    token: null,
+    movies: [],
+  },
+  getters: {
+    isLogIn(state) {
+      return state.token ? true : false
+    }
+  },
+  mutations: {
+    // 회원가입 && 로그인
+    SAVE_TOKEN(state, token) {
+      state.token = token
+      router.push({ name: 'home' })
+    },
+    GET_MOVIES(state, movies) {
+      state.movies = movies
+    },
+  },
+  actions: {
+    signUp(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/signup/`,
+        data: {
+          username: payload.username,
+          password1: payload.password1,
+          password2: payload.password2,
+        }
+      })
+      .then((res) => {
+        context.commit('SAVE_TOKEN', res.data.key)
+      })
+      .catch(err => console.log(err))
+    },
+    logIn(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/login/`,
+        data: {
+          username: payload.username,
+          password: payload.password,
+        }
+      })
+      .then((res) => {
+        // console.log(res)
+        context.commit('SAVE_TOKEN', res.data.key)
+      })
+    },
+    getMovies(context) {
+      axios({
+        method: 'get',
+        url: `${API_URL}/api/v1/movies/`,
+        headers: {
+          Authorization: `Token ${context.state.token}`
+        }
+      })
+      .then((res) => {
+        context.commit('GET_MOVIES', res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  },
   modules: {
-    accounts,
   }
 })
+
+
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+
+// import accounts from '@/store/modules/accounts'
+
+// Vue.use(Vuex)
+
+// export default new Vuex.Store({
+//   modules: {
+//     accounts,
+//   }
+// })
