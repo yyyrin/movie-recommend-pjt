84218c7b0827a51819a1b5e32cec12b7ed1a1b16
boham97 <kangbosung97@gmail.com>
2022-11-19 20:04:05 +0900

    커뮤니티 완성

--- final-pjt-back/community/serializers.py
+++ final-pjt-back/community/serializers.py
@@ -9,12 +9,28 @@ class CommunityListSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('user',)
 
-class ArticleSerializer(serializers.ModelSerializer):
 
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('article','user',)
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('pk', 'username')
+    comment_set = CommentSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True)
+    community = CommunityListSerializer(read_only= True)
+    like_users = UserSerializer(many=True, read_only=True)
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('community','user')
+
 
 class CommuntiySerializer(serializers.ModelSerializer):
     class UserSerializer(serializers.ModelSerializer):
