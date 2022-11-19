f6a0c02ab55bca2028f5523773a6bb2044bbc4fb
최예린 <qsc130@gmail.com>
2022-11-19 16:35:07 +0900

    community create

---
+++ final-pjt-front/src/components/Community/CreateCommunity.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      새로 만들기
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="mb-3">
+              <label for="name" class="col-form-label">커뮤니티명:</label>
+              <input type="text" class="form-control" id="name" v-model.trim="name">
+            </div>
+            <div class="mb-3">
+              <label for="community-thumbnail" class="col-form-label">썸네일:</label>
+              <input type="text" class="form-control" id="community-thumbnail" v-model.trim="thumbnail">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" @click="createCommunity">생성</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const API_URL = 'http://127.0.0.1:8000'
+
+export default {
+  name: 'CreateCommunity',
+  data() {
+    return {
+      name: null,
+      thumbnail: null,
+    }
+  },
+  methods: {
+    createCommunity() {
+      const name = this.name
+      const thumbnail = this.thumbnail
+      if (!name) {
+        alert('커뮤니티명을 입력해주세요')
+        return
+      } else if (!thumbnail) {
+        alert('썸네일을 넣어주세요')
+        return
+      }
+      axios({
+        method: 'post',
+        url: `${API_URL}/api/v1/community/`,
+        data: {
+          name: name,
+          thumbnail: thumbnail,
+        },
+        headers: {
+          Authorization: `Token ${this.$store.state.token}`
+        }
+      })
+      .then((res) => {
+        console.log(res)
+        this.$router.push({ name: 'community' })
+      })
+      .catch((err) => {
+        console.log(err)
+      }) 
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- final-pjt-front/src/views/CommunityView.vue
+++ final-pjt-front/src/views/CommunityView.vue
@@ -2,19 +2,22 @@
   <div>
     <nav-bar></nav-bar>
     <h1>CommunityView</h1>
+    <CreateCommunity/>
     <CommunityCards/>
   </div>
 </template>
                  
 <script>
 import NavBar from '@/components/templates/NavBar'
+import CreateCommunity from '@/components/Community/CreateCommunity'
 import CommunityCards from '@/components/Community/CommunityCards'
 
 export default {
   name: 'CommunityView',
   components: {
     NavBar,
-    CommunityCards
+    CreateCommunity,
+    CommunityCards,
   },
   methods: {
     getCommunities() {
