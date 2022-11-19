2752839fd1c714dfb464e01bbb8c5257c6f87b92
boham97 <kangbosung97@gmail.com>
2022-11-19 16:32:23 +0900

    커뮤니티 수정및 아티클 수정

--- final-pjt-back/community/serializers.py
+++ final-pjt-back/community/serializers.py
@@ -7,21 +7,21 @@ class CommunityListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Community
         fields = '__all__'
-        read_only_fields = ('user')
+        read_only_fields = ('user',)
 
-class ArtcleSerializer(serializers.ModelSerializer):
+class ArticleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('community')
+        read_only_fields = ('community','user')
 
 class CommuntiySerializer(serializers.ModelSerializer):
     class UserSerializer(serializers.ModelSerializer):
         class Meta:
             model = get_user_model()
             fields = ('pk', 'username')
-    article_set = ArtcleSerializer(many=True, read_only=True)
+    article_set = ArticleSerializer(many=True, read_only=True)
     user = UserSerializer(read_only=True)
 
     class Meta:
--- final-pjt-back/community/views.py
+++ final-pjt-back/community/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Article, Comment, Community
 from django.shortcuts import get_object_or_404, get_list_or_404
-from .serializers import CommunityListSerializer, CommuntiySerializer, ArtcleSerializer
+from .serializers import CommunityListSerializer, CommuntiySerializer, ArticleSerializer
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 
@@ -23,3 +23,56 @@ def community_list(request):
         if serializer.is_valid(raise_exception=True):
             serializer.save(user = request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+#@permission_classes([IsAuthenticated])
+def community_detail(request, community_pk):
+    print(request)
+    community = get_object_or_404(Community, pk=community_pk)
+
+    if request.method == 'GET':
+        serializer = CommuntiySerializer(community)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE' and request.user == community.user:
+        community.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT' and request.user == community.user:
+        serializer = CommuntiySerializer(community, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+#@permission_classes([IsAuthenticated])
+def article_detail(request, community_pk, article_pk):
+    communtiy = get_object_or_404(Community, pk=community_pk)
+    article = get_object_or_404(Article, pk=article_pk)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE' and (article.user == request.user or communtiy.user == article.user):
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT' and (article.user == request.user or communtiy.user == article.user):
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+@api_view(['POST'])
+#@permission_classes([IsAuthenticated])
+def article_create(request, community_pk):
+    community = get_object_or_404(Community, pk=community_pk)
+    serializer = ArticleSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user, community=community)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
