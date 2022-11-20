6ae3699955dd344395bf29344da516f8e8c8c5b6
최예린 <qsc130@gmail.com>
2022-11-20 17:46:59 +0900

    커뮤니티 내 article 목록, article create 구현

--- final-pjt-front/src/views/CommunityArticleView.vue
+++ final-pjt-front/src/views/CommunityArticleView.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="community-article-view">
     <nav-bar></nav-bar>
-    <h1>CommunityArticleView</h1>
+    <p>이름: {{ community?.name }}</p>
+    <p>썸네일 넣을 자리: {{ community?.thumbnail }}</p>
+    <router-link :to="{ name: 'community_article_create' }">
+      [CREATE]
+    </router-link>
     <ul class="list-group list-group-flush">
-      <CommunityArticleList/>
+      <CommunityArticleList
+        :community_id="community?.id"
+      />
     </ul>
   </div>
 </template>
@@ -11,45 +17,58 @@
 <script>
 import NavBar from '@/components/templates/NavBar'
 import CommunityArticleList from '@/components/Community/CommunityArticleList'
-// import axios from 'axios'
+import axios from 'axios'
 
-// const API_URL = 'http://127.0.0.1:8000' 
+const API_URL = 'http://127.0.0.1:8000' 
 
 export default {
   name: 'CommunityArticleView',
-  // data() {
-  //   return {
-  //     articles: null,
-  //   }
-  // },
   components: {
     NavBar,
     CommunityArticleList,
   },
-  computed: {
-    isLogIn() {
-      return this.$store.getters.isLogin
+  data() {
+    return {
+      community: null,
     }
   },
-  // created() {
-  //   this.getArticles()
-  // },
-  // method: {
-  //   getArticles() {
-  //     axios({
-  //       method: 'get',
-
-  //     })
-  //   }
-  // },
   // computed: {
-  //   articles() {
-  //     return this.$store.state.articles
-  //     // return this.$store.state.articles.filter((article) => {
-  //     //   return article.community_id === 
-  //     // })
+  //   isLogIn() {
+  //     return this.$store.getters.isLogIn
   //   }
   // },
+  created() {
+    this.getCommunityDetail()
+    this.getArticles()
+  },
+  methods: {
+    getCommunityDetail() {
+      axios({
+        method: 'get',
+        url: `${API_URL}/api/v1/community/${this.$route.params.community_id}/`
+      })
+      .then((res) => {
+        // console.log(res)
+        this.community = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+    getArticles() {
+      this.$store.dispatch('getArticles', `${this.$route.params.community_id}`)
+      // axios({
+      //   method: 'get',
+      //   url: `${API_URL}/api/v1/community/${this.$route.params.community_id}/`,
+      // })
+      // .then((res) => {
+      //   console.log(res)
+      // })
+      // .catch((err) => {
+      //   console.log(err)
+      // })
+    }
+  },
 }
 </script>
 
