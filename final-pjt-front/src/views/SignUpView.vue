8d4da1a0c59b5bbe996952ad71a9daf23b5ce384
최예린 <qsc130@gmail.com>
2022-11-25 04:56:52 +0900

    login & logout

--- final-pjt-front/src/views/SignUpView.vue
+++ final-pjt-front/src/views/SignUpView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <div id="signup">
+
+    <div class="container">
+      <div class="card">
+        <div class="card_title">
+          <h1>Sign Up</h1>
+          <span>이미 계정이 있으신가요? <router-link :to="{ name: 'login' }">Login</router-link></span>
+        </div>
+        <div class="form">
+        <form @submit.prevent="signUp">
+          <input type="text" name="username" id="username" placeholder="UserName" v-model="username" required/>
+          <input type="password" name="password1" placeholder="Password" id="password1" v-model="password1" required/>
+          <input type="password" name="password2" placeholder="Password" id="password2" v-model="password2" required/>
+          <button>Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <div>
     <h1>Sign Up Page</h1>
     <form @submit.prevent="signUp">
 
@@ -14,7 +33,7 @@
       
       <input type="submit" value="SignUp">
     </form>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -56,5 +75,86 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+.container {
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  background-image: radial-gradient(
+    circle farthest-corner at 10% 20%,
+    rgba(252, 140, 55, 0.921) 0%,
+    rgb(253, 232, 210) 90%
+  );
+}
+
+.card {
+  border-radius: 10px;
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.3);
+  width: 400px;
+  height: 400px;
+  background-color: #ffffff;
+  padding: 10px 30px;
+}
+
+.card_title {
+  text-align: center;
+  padding: 10px;
+}
+
+.card_title h1 {
+  font-size: 26px;
+  font-weight: bold;
+}
+
+.form input {
+  margin: 10px 0;
+  width: 100%;
+  background-color: #e2e2e2;
+  border: none;
+  outline: none;
+  padding: 12px 20px;
+  border-radius: 4px;
+}
+
+.form button {
+  background-color: #4796ff;
+  color: #ffffff;
+  font-size: 16px;
+  outline: none;
+  border-radius: 5px;
+  border: none;
+  padding: 8px 15px;
+  width: 100%;
+}
+
+.card_terms {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+
+.card_terms input[type="checkbox"] {
+  background-color: #e2e2e2;
+}
+
+.card_terms span {
+  margin: 5px;
+  font-size: 13px;
+}
+
+.card a {
+  color: #4796ff;
+  text-decoration: none;
+}
 
 </style>
